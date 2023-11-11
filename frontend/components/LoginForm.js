a35dda597a7d5e4983abb5ba00bfe3c76b9b0d10
Ty Solomon <tyksully@gmail.com>
2023-11-10 17:07:38 -0700

    finished project

--- frontend/components/LoginForm.js
+++ frontend/components/LoginForm.js
@@ -1,13 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PT from 'prop-types'
 
 const initialFormValues = {
   username: '',
   password: '',
 }
+
 export default function LoginForm(props) {
-  const [values, setValues] = useState(initialFormValues)
+  const [values, setValues] = useState(initialFormValues);
+  const [disabled, setDisabled] = useState(true)
   // ✨ where are my props? Destructure them here
+  const { login } = props;
 
   const onChange = evt => {
     const { id, value } = evt.target
@@ -17,14 +20,26 @@ export default function LoginForm(props) {
   const onSubmit = evt => {
     evt.preventDefault()
     // ✨ implement
+    login(values);
+    setValues(initialFormValues);
   }
+  // const isDisabled = () => { //!changed from this 
+  //   const trimmedUsername = values.username.trim();
+  //   const trimmedPassword = values.password.trim();
+  //   console.log(String(trimmedPassword).length)
+  //   // ✨ implement
+  //   // Trimmed username must be >= 3, and
+  //   // trimmed password must be >= 8 for
+  //   // the button to become enabled
+  // }
 
-  const isDisabled = () => {
-    // ✨ implement
-    // Trimmed username must be >= 3, and
-    // trimmed password must be >= 8 for
-    // the button to become enabled
-  }
+  useEffect(() => {
+    const trimmedUsername = values.username.trim();
+    const trimmedPassword = values.password.trim();
+    if (trimmedUsername.length >= 3 && trimmedPassword.length >= 8) {
+      setDisabled(false); 
+    }
+  }, [values])
 
   return (
     <form id="loginForm" onSubmit={onSubmit}>
@@ -43,7 +58,7 @@ export default function LoginForm(props) {
         placeholder="Enter password"
         id="password"
       />
-      <button disabled={isDisabled()} id="submitCredentials">Submit credentials</button>
+      <button disabled={disabled} id="submitCredentials">Submit credentials</button>
     </form>
   )
 }
--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,11 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import Spinner from "./Spinner"
+import { render } from "@testing-library/react"
+import React from "react"
+import "@testing-library/jest-dom"
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  const {queryByText} = render(<Spinner on={true}/>)
+  expect(queryByText(/please wait.../i)).toBeInTheDocument()
 })
